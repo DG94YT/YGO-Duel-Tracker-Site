4547fc988c9c66ae5b8eff2ca0c970fdf23902f8
DG94YT <72869243+DG94YT@users.noreply.github.com>
2025-11-08 20:57:32 +0800

    Fixed add/subtract toggle for text.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Yu-Gi-Oh! Duel Tracker HTML5
 
 A Duel Tracker for the Yu-Gi-Oh! card game, written in HTML5 and JS.
+Try it [here](https://dg94yt.github.io/YGO-Duel-Tracker-Site/src/index.html)!
--- src/script/script.js
+++ src/script/script.js
@@ -5,8 +5,24 @@ function toggleAdd() {
 	isAdding = !isAdding;
 	console.log("isAdding: ", isAdding);
 
-	const btn_1000 = document.getElementById("btn1000");
-	btn_1000.textContent = isAdding ? "-1000" : "+1000";
+	const updateButtons = (className, amount) => {
+
+		const buttons = document.getElementsByClassName(className);
+		for (let btn of buttons) {
+			btn.textContent = isAdding ? `+${amount}` : `-${amount}`;
+		}
+	};
+
+	updateButtons("btn1000", 1000);
+	updateButtons("btn500", 500);
+	updateButtons("btn100", 100);
+	updateButtons("btn50", 50);
+
+	let p1Custom = document.getElementById("customP1");
+	let p2Custom = document.getElementById("customP2");
+
+	p1Custom.innerText = isAdding ? "Custom number (+)" : "Custom number (-)";
+	p2Custom.innerText = isAdding ? "Custom number (+)" : "Custom number (-)";
 }
 
 function modifyLP(inputID, amount) {
@@ -51,4 +67,26 @@ function resetAllLP(inputID, input2ID) {
 	const input2Field = document.getElementById(input2ID);
 	inputField.value = 8000;
 	input2Field.value = 8000;
+}
+
+function closePanel(elementID) {
+
+	document.getElementById(elementID).style.display = "none";
+}
+
+function openPanel(elementID) {
+
+	document.getElementById(elementID).style.display = "block";
+}
+
+function tossCoin() {
+
+	const result = Math.random() < 0.5 ? "Heads" : "Tails";
+	alert("The coin toss result is ", result);
+}
+
+function rollDie() {
+
+	const result = Math.floor(Math.random() * 6) + 1;
+	alert("Die rolled a ", result);
 }
